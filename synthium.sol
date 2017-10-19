496afdecef4b66f724c5ed893aee7e3f610a6298
synthium2017 <32312938+synthium2017@users.noreply.github.com>
2017-10-18 22:01:05 -0400

    Add files via upload

--- synthium.sol
+++ synthium.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.16;
+pragma solidity 0.4.17;
 contract Ownable {
     address public owner;
 
@@ -7,6 +7,7 @@ contract Ownable {
     }
     modifier onlyOwner() { //This modifier is for checking owner is calling
         if (owner == msg.sender) {
+            _;
         } else {
             revert();
         }
